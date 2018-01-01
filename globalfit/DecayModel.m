54e7df92f085e2e06ccb2376f13eaa385b398bf4
R. Patrick Xian <xrpatrick@gmail.com>
2018-01-01 18:31:41 +0100

    Added CI calculation and generalize to n traces

--- globalfit/DecayModel.m
+++ globalfit/DecayModel.m
@@ -1,13 +1,15 @@
-function fitfunc_flat = DecayModel(pars, x)
+function fitfunc_flat = DecayModel(pars, x, ntrace)
+    % Exponential decay model constructor
     
-    params = reshape(pars(1:39), 13, 3);
+    nvecparam = ntrace*3;
+    params = reshape(pars(1:nvecparam), ntrace, 3);
     a1 = params(:,1);
     a2 = params(:,2);
     z = params(:,3);
-    c1 = pars(40);
-    c2 = pars(41);
-    d = pars(42);
-    m = pars(43);
+    c1 = pars(nvecparam+1);
+    c2 = pars(nvecparam+2);
+    d = pars(nvecparam+3);
+    m = pars(nvecparam+4);
     
     term1 = (a1.*exp(-x/c1))*exp(m/c1+(d^2)/(2*c1^2)).*(1+erf((x-(m+d^2/c1))/(sqrt(2)*d)));
     term2 = (a2.*exp(-x/c2))*exp(m/c2+(d^2)/(2*c2^2)).*(1+erf((x-(m+d^2/c2))/(sqrt(2)*d)));
--- globalfit/gfit.m
+++ globalfit/gfit.m
@@ -1,6 +1,6 @@
 % Preparations for fitting
 % Prepare fitting data
-datamat = redmat;
+datamat = relc';
 dtaxis = taxis - taxis(5);
 [nt, np] = size(datamat);
 datamat_flat = reshape(datamat, 1, numel(datamat));
@@ -16,42 +16,49 @@ inits_flat(end+1:end+4) = [1e6 300 200 200];  % Append scalar variables at the e
 lb = ones(np, nparamdim)*(-Inf);
 lb(:, nparamdim-1) = -0.006;
 lb_flat = reshape(lb, 1, numel(lb));
-lb_flat(end+1:end+4) = [-Inf, -Inf, 0, -Inf];
+lb_flat(end+1:end+4) = [-Inf, -Inf, 150, -Inf];
 
 ub = ones(np, nparamdim)*Inf;
 ub(:, nparamdim-1) = 0.006;
 ub_flat = reshape(ub, 1, numel(lb));
-ub_flat(end+1:end+4) = [Inf, Inf, Inf, Inf];
+ub_flat(end+1:end+4) = [Inf, Inf, 250, Inf];
 
 % Additional fitting parameters and conditions
 ops = optimset('Display','iter');
 
 %%
 % % Model test
-% out = DecayModel(inits_flat, dtaxis);
+% out = DecayModel(inits_flat, dtaxis, np);
 % size(out)
 
 %%
 % Perform fit
 [fitvals,resnorm,res,exitflag,output,lambda,jac] = ...
-    lsqcurvefit(@(params,dtaxis)DecayModel(params,dtaxis),...
+    lsqcurvefit(@(params,dtaxis)DecayModel(params,dtaxis,np),...
     inits_flat, dtaxis, datamat_flat, lb_flat, ub_flat, ops);
 
+%%
 % Extract fitting results
-fitmat = reshape(fitvals(1:39), np, floor(numel(fitvals)/np));
-a1_fit = fitmat(:,1);
-a2_fit = fitmat(:,2);
-c1_fit = fitvals(40);
-c2_fit = fitvals(41);
-d_fit = fitvals(42);
-m_fit = fitvals(43);
-z_fit = fitmat(:,3);
+idx = [1:floor(numel(fitvals)/np)]*np;
+a1_fit = fitvals(1:idx(1));
+a2_fit = fitvals(idx(1)+1:idx(2));
+z_fit = fitvals(idx(2)+1:idx(3));
+
+c1_fit = fitvals(idx(3)+1);
+c2_fit = fitvals(idx(3)+2);
+d_fit = fitvals(idx(3)+3);
+m_fit = fitvals(idx(3)+4);
+
+% Construct fitting parameter dictionary
+varlib = dictConstructor({}, {'a1',a1_fit},{'a2',a2_fit},{'z',z_fit},...
+    {'c1',c1_fit},{'c2',c2_fit},{'d',d_fit},{'m',m_fit});
+
 fitdyn = DecayModel(fitvals, dtaxis);
 fitdyn = reshape(fitdyn, nt, np);
 
 %%
 % Display fitting results
-for icurv = 1:np
+for icurv = 1:50
     figure;
     hold on
     plot(dtaxis, datamat(:,icurv))
@@ -68,3 +75,22 @@ end
 %%
 % Calculate confidence interval
 ci = nlparci(fitvals,res,'Jacobian',jac);
+
+%%
+% Restructure the ci matrix and append to varlib
+a1_ci = ci(1:idx(1),:)';
+a2_ci = ci(idx(1)+1:idx(2),:)';
+z_ci = ci(idx(2)+1:idx(3),:)';
+
+c1_ci = ci(idx(3)+1,:);
+c2_ci = ci(idx(3)+2,:);
+d_ci = ci(idx(3)+3,:);
+m_ci = ci(idx(3)+4,:);
+
+varlib = dictConstructor(varlib, {'a1_ci',a1_ci},{'a2_ci',a2_ci},{'z_ci',z_ci},...
+    {'c1_ci',c1_ci},{'c2_ci',c2_ci},{'d_ci',d_ci},{'m_ci',m_ci});
+
+%%
+% Save global fitting results
+save('2016-12-14_Scan_34_gfit.mat','redmat','dtaxis','inits_flat','fitvals',...
+    'lb_flat','ub_flat','fitdyn','varlib','output','res','jac')
